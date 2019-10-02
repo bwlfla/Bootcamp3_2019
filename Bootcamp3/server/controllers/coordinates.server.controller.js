f17a4c28ac749710583727f4adb17ac8348d1656
Brian Lu <brian.lu@ufl.edu>
2019-10-02 18:37:42 -0400

    Using the opencage API successfult

--- Bootcamp3/server/controllers/coordinates.server.controller.js
+++ Bootcamp3/server/controllers/coordinates.server.controller.js
@@ -14,7 +14,7 @@ module.exports = function(req, res, next) {
     //Setup your options q and key are provided. Feel free to add others to make the JSON response less verbose and easier to read 
     var options = { 
       q: addressTemp4,
-      key: config.openCage.key,  
+      key: config.openCage.key,
     }
 
     //Setup your request using URL and options - see ? for format
@@ -23,9 +23,9 @@ module.exports = function(req, res, next) {
       qs: options
       }, function(error, response, body) {
         //For ideas about response and error processing see https://opencagedata.com/tutorials/geocode-in-nodejs
-        
+        if(error) throw error;
         //JSON.parse to get contents. Remember to look at the response's JSON format in open cage data
-        
+        var contents=JSON.parse(body);
         /*Save the coordinates in req.results -> 
           this information will be accessed by listings.server.model.js 
           to add the coordinates to the listing request to be saved to the database.
@@ -33,6 +33,7 @@ module.exports = function(req, res, next) {
           Assumption: if we get a result we will take the coordinates from the first result returned
         */
         //  req.results = stores you coordinates
+        req.results=contents.results[0].geometry;
         next();
     });
   } else {
